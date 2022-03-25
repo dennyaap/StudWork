034f78994f839b47944bb04fabc70dbae8379538
Denis Alekhin <alekhin.den69@gmail.com>
2022-03-25 20:44:58 +0500

    added class category

--- app/components/navbar_component.php
+++ app/components/navbar_component.php
@@ -1,10 +1,10 @@
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
-  <div class="container">
+<nav class="navbar navbar-expand-sm navbar-light">
+  <div class="container navbar-container">
     <a href="#" class="navbar-brand"><img id="logo" src="/app/public/images/logo.svg" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarContent">
+    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
           <a href="#" class="nav-link">Главная</a>
@@ -16,6 +16,11 @@
           <a href="#" class="nav-link">О нас</a>
         </li>
       </ul>
+
+      <div class="buttons">
+        <button class="btn" id="btn-resume">Создать резюме</button>
+        <button class="btn" id="btn-auth">Войти</button>
+      </div>
     </div>
   </div>
 </nav>
---
+++ app/models/Category.php
@@ -0,0 +1,41 @@
+<?php
+
+// namespace App\models;
+
+// use App\services\Connection;
+
+// class Category
+// {
+//     public static function pdo($config = CONFIG_CONNECTION)
+//     {
+//         return Connection::make($config)
+//     }
+// }
+
+namespace App\models;
+
+use App\services\Connection;
+
+class Category
+{
+    public static function pdo($config = CONFIG_CONNECTION)
+    {
+        return Connection::make($config);
+    }
+    public static function showCategories()
+    {
+        $stmt = self::pdo()->query('SELECT * FROM categories');
+        return $stmt->fetchAll();
+    }
+    public static function showCategory($id)
+    {
+        $stmt = self::pdo()->prepare('SELECT vacancies.*, categories.name as category FROM vacancies
+        INNER JOIN categories ON vacancies.category_id = categories.id
+        WHERE vacancies.category_id = :id');
+
+        $stmt->execute(
+            ['id' => $id]
+        );
+        return $stmt->fetchAll();
+    }
+}
