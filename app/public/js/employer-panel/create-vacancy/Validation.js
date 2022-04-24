28a8316e4d4bc8d05c6082f24f0837fd10623070
Denis Alekhin <alekhin.den69@gmail.com>
2022-04-24 18:11:28 +0500

    added preview vacancy

--- app/public/js/employer-panel/create-vacancy/Validation.js
+++ app/public/js/employer-panel/create-vacancy/Validation.js
@@ -1,11 +1,17 @@
 class Validation{
-    static checkErrors(vacancyName, categoryName, nameOrganization){
+    static checkErrors(vacancyName, categoryName, nameOrganization, desctription){
         let errors = [];
         if(vacancyName == ''){
             errors.push('Введите название вакансии');
         }
+        else if (nameOrganization == ''){
+            errors.push('Введите название организации');
+        }
         else if (categoryName == ''){
             errors.push('Введите название Категории');
+        }
+        else if(desctription == ''){
+            errors.push('Введите описание вакансии')
         }
         return errors;
     }
